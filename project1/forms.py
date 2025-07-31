5655f6ee375857b6cf33e16b51c5d74d9f0e047a
user1 <qwerty@gmail.com>
2025-07-31 07:29:49 +0500

    css avtosalon

--- project1/forms.py
+++ project1/forms.py
@@ -4,4 +4,27 @@ from .models import *
 class AvtoSalonForms(forms.ModelForm):
     class Meta:
         model = Autosalon
-        fields = ['title','context','email','phone','address']
+        fields = ['title', 'context', 'email', 'phone', 'address']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Salon nomini kiriting'
+            }),
+            'context': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Salon haqida ma\'lumot'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Email manzili'
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Telefon raqami'
+            }),
+            'address': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Manzil'
+            }),
+        }
